506b2f6290fb547371e53efe0c8d8621723a4827
pat473 <pat0473p@gmail.com>
2026-07-29 23:56:34 +0200

    Dalsze dekodowanie, ulepszenie wspolnych metod, mysl o wspolnym menu

--- dekodowanie.py
+++ dekodowanie.py
@@ -1,11 +1,14 @@
 from stale import *
+from wspolne import SzablonQR, MetodyMasek
 
-class Dekoder:
+
+class DeMajsterMatrycy:
     def __init__(self, gotowa_matryca):
         self.matryca = gotowa_matryca
         self.usun_strefe_ochronna()
         self.rozmiar = len(gotowa_matryca)
         self.wersja = int(((self.rozmiar-21)/4)+1)
+        self.wzorzec_matryca = SzablonQR(self.rozmiar,self.wersja).wygeneruj()
 
         self.poziom_korekcji = None
         self.nr_maski = None
@@ -26,11 +29,11 @@ class Dekoder:
                 roznic +=1
         return roznic
 
-    def sprawdzanie_bitow_korekcyjnych(self, odczytane_bity):
+    def sprawdzanie_bitow_korekcyjnych(self, odczytane_bity, slownik):
         najlepsza_wersja = None
         najmniejsza_odleglosc = 999
 
-        for wersja, idealne_bity in ciag_bitow_dla_danej_wersji.items():
+        for wersja, idealne_bity in slownik.items():
             odleglosc = self._oblicz_odleglosc_hamminga(odczytane_bity, idealne_bity)
 
             if odleglosc < najmniejsza_odleglosc:
@@ -38,43 +41,89 @@ class Dekoder:
                 najlepsza_wersja = wersja
         return najlepsza_wersja, najmniejsza_odleglosc
 
+    def _wybierz_zwyciezce(self, ciag_1, ciag_2, slownik):
+        wynik_1, bledy_1 = self.sprawdzanie_bitow_korekcyjnych(ciag_1, slownik)
+        wynik_2, bledy_2 = self.sprawdzanie_bitow_korekcyjnych(ciag_2, slownik)
 
-    def odczytaj_info_o_wersji(self):
-        if self.wersja >= 7:
-            info_lewy_dol = ""
-            for i in range(6):
-                for j in range(3):
-                    bit = str(self.matryca[self.rozmiar-11+j][i])
-                    info_lewy_dol += bit
-            info_prawa_gora = ""
-            for i in range(6):
-                for j in range(3):
-                    bit = str(self.matryca[i][self.rozmiar-11+j])
-                    info_prawa_gora += bit
+        if bledy_1 <= bledy_2 and bledy_1 <= 3:
+            return wynik_1
+        elif bledy_2 <= bledy_1 and bledy_2 <=3:
+            return wynik_2
+        else:
+            raise ValueError("Kod jest zbyt uszkodzony.")
 
-            wersja_lewy_dol, bledy_lewy_dol = self.sprawdzanie_bitow_korekcyjnych(info_lewy_dol)
-            wersja_prawa_gora, bledy_prawa_gora = self.sprawdzanie_bitow_korekcyjnych(info_prawa_gora)
-
-            if bledy_lewy_dol <= bledy_prawa_gora and bledy_lewy_dol<=3:
-                return wersja_lewy_dol
-            elif bledy_prawa_gora <= bledy_lewy_dol and bledy_prawa_gora<=3:
-                return wersja_prawa_gora
-            else:
-                raise TypeError("Kod jest zbyt uszkodzony.")
-
-    def odczytaj_info_poziom_korekcji_i_maska(self):
+    def odczytaj_dane(self):
         info_laczne = ""
         info_rozdzielne = ""
 
+        for i in range(9):
+            if i != 6:
+                bit = str(self.matryca[8][i])
+                info_laczne += bit
+        for i in range(7,-1,-1):
+            if i != 6 :
+                bit = str(self.matryca[i][8])
+                info_laczne += bit
+        for i in range(7):
+            bit = str(self.matryca[self.rozmiar-1-i][8])
+            info_rozdzielne += bit
+        for i in range(7,-1,-1):
+            bit = str(self.matryca[8][self.rozmiar-1-i])
+            info_rozdzielne += bit
+
+        format_wynik = self._wybierz_zwyciezce(info_laczne, info_rozdzielne,ciag_bitow_dla_maski_oraz_poziomu_korekcji_bledow)
+        self.poziom_korekcji = format_wynik[0]
+        self.nr_maski = format_wynik[1]
+
+        if self.wersja >=7:
+            info_lewy_dol = ""
+            info_prawa_gora = ""
+            for i in range(6):
+                for j in range(3):
+                    bit_ld = str(self.matryca[self.rozmiar - 11 + j][i])
+                    info_lewy_dol += bit_ld
+                    bit_pg = str(self.matryca[i][self.rozmiar-11+j])
+                    info_prawa_gora += bit_pg
+
+            self.wersja = self._wybierz_zwyciezce(info_lewy_dol, info_prawa_gora, ciag_bitow_dla_danej_wersji)
+
+    def odmaskowanie(self):
+        mapa_samych_danych = self.wzorzec_matryca.strefa_danych
+        for y,x in mapa_samych_danych:
+            if MetodyMasek.czy_odwrocic_bit(self.nr_maski,y,x):
+                self.matryca[y][x] ^=1
+
+    def odczytanie_ciagu_danych(self):
+        generator = SzablonQR(self.rozmiar,self.wersja)
+        trasa = generator.pobierz_trase_wezyka2()
+        odcztane_dane = ""
+        for y,x in trasa:
+            bit = str(self.matryca[y][x])
+            odcztane_dane += bit
+        return odcztane_dane
+
+    def zbierz_dane_z_matrycy(self):
+        self.odczytaj_dane()
+        self.odmaskowanie()
+        return self.odczytanie_ciagu_danych()
+
+
+    # def zamien_obraz_na_matryce(self):
+
+class DekoderDanych:
+
+    def __init__(self, ciag_danych, wersja, poziom_korekcji):
+        self.ciag_danych = ciag_danych
+        self.wersja = wersja
+        self.poziom_korekcji = poziom_korekcji
 
 
 
+        self.odczytany_tekst = ""
 
 
 
+    def antyprzeplot(self):
 
-
-
-
-
+    def podzial_na_liczby(self):
 
---
+++ wspolne.py
@@ -0,0 +1,170 @@
+from stale import *
+
+class SzablonQR:
+    def __init__(self,rozmiar, wersja,):
+        self.rozmiar = rozmiar
+        self.wersja = wersja
+        self.szablon = []
+        self.tworzenie_pustej_matrycy()
+        self.strefa_danych = []
+
+    def tworzenie_pustej_matrycy(self):
+        for y in range(self.rozmiar):
+            wiersz = []
+            for x in range(self.rozmiar):
+                wiersz.append(None)
+            self.szablon.append(wiersz)
+
+    def wygeneruj(self):
+        self.tworzenie_wzorcow_wyszukiwania()
+        self.tworzenie_separatorow()
+        self.tworzenie_wzorcow_wyrownania()
+        self.tworzenie_wzorcow_czasowych()
+        self.tworzenie_ciemnego_modulu()
+        self.tworzenie_miejsc_zarezerwowanych()
+        self.zapamietanie_strefy_danych()
+
+        return self.szablon
+
+    def _wklej_dany_wzor(self,wzor, start_x, start_y):
+        wysokosc = len(wzor)
+        szerokosc = len(wzor[0])
+        for y in range(wysokosc):
+            for x in range(szerokosc):
+                self.szablon[start_y+y][start_x+x] = wzor[y][x]
+
+    def tworzenie_wzorcow_wyszukiwania(self):
+        wzor = wzor_wyszukiwajacy
+        self._wklej_dany_wzor(wzor,0,0)
+        self._wklej_dany_wzor(wzor,self.rozmiar -7,0)
+        self._wklej_dany_wzor(wzor,0,self.rozmiar-7)
+
+    def tworzenie_separatorow(self):
+        for i in range(8):
+            self.szablon[7][i] = 0
+            self.szablon[i][7] = 0
+
+            self.szablon[7][self.rozmiar-1-i] = 0
+            self.szablon[i][self.rozmiar-8] = 0
+
+            self.szablon[self.rozmiar-1-i][7] = 0
+            self.szablon[self.rozmiar-8][i] = 0
+
+    def tworzenie_wzorcow_wyrownania(self):
+        if self.wersja ==1:
+            return
+
+        wspolrzedne = slownik_wzorcow_wyrownania[self.wersja]
+
+        for srodek_x in wspolrzedne:
+            for srodek_y in wspolrzedne:
+
+                if srodek_x < 10 and srodek_y < 10:
+                    continue
+                if srodek_x > self.rozmiar - 10 and srodek_y<10:
+                    continue
+                if srodek_x < 10 and srodek_y > self.rozmiar - 10:
+                    continue
+
+                start_x = srodek_x -2
+                start_y = srodek_y -2
+
+                self._wklej_dany_wzor(wzor_wyrownania,start_x,start_y)
+
+
+    def tworzenie_wzorcow_czasowych(self):
+        pasek_poziomy_start_x = 8
+        pasek_poziomy_start_y = 6
+        pasek_pionowy_start_x = 6
+        pasek_pionowy_start_y = 8
+        dlugosc_paska = self.rozmiar - 16
+        czarne_pole = True
+        for i in range(dlugosc_paska):
+            if czarne_pole:
+                self.szablon[pasek_poziomy_start_y][pasek_poziomy_start_x+i] = 1
+                self.szablon[pasek_pionowy_start_y+i][pasek_pionowy_start_x] = 1
+            else:
+                self.szablon[pasek_poziomy_start_y][pasek_poziomy_start_x+i] = 0
+                self.szablon[pasek_pionowy_start_y+i][pasek_pionowy_start_x] = 0
+            czarne_pole = not czarne_pole
+
+    def tworzenie_ciemnego_modulu(self):
+        self.szablon[(4*self.wersja)+9][8] = 1
+
+    def bezpieczna_rezerwacja(self,y,x):
+        if self.szablon[y][x] is None:
+            self.szablon[y][x] = -1
+
+    def tworzenie_miejsc_zarezerwowanych(self):
+        for i in range(9):
+            self.bezpieczna_rezerwacja(8,i)
+            self.bezpieczna_rezerwacja(i,8)
+
+        for i in range(8):
+            self.bezpieczna_rezerwacja(8,self.rozmiar-1-i)
+            self.bezpieczna_rezerwacja(self.rozmiar-1-i,8)
+
+        if self.wersja >= 7:
+            for i in range(3):
+                for j in range(6):
+                    self.bezpieczna_rezerwacja(j,self.rozmiar-9-i)
+                    self.bezpieczna_rezerwacja(self.rozmiar-9-i,j)
+
+    def zapamietanie_strefy_danych(self):
+        for y in range(len(self.szablon)):
+            for x in range(len(self.szablon)):
+                if self.szablon[y][x] is None:
+                    self.strefa_danych.append((y,x))
+
+    def pobierz_trase_wezyka(self, czy_w_gore, indeks_x):
+        trasa_odcinka = []
+        if czy_w_gore:
+            for y in range(self.rozmiar-1,-1,-1):
+                for i in range(2):
+                    aktualny_x = indeks_x - i
+                    if self.szablon[y][aktualny_x] is None:
+                        trasa_odcinka.append((y, aktualny_x))
+        else:
+            for y in range(0, self.rozmiar,1):
+                for i in range(2):
+                    aktualny_x = indeks_x - i
+                    if self.szablon[y][aktualny_x] is None:
+                        trasa_odcinka.append((y,aktualny_x))
+
+        return trasa_odcinka, not czy_w_gore
+
+    def pobierz_trase_wezyka2(self):
+        indeks_x = self.rozmiar-1
+        czy_w_gore = True
+        pelna_trasa = []
+        while indeks_x >=0:
+            if indeks_x ==6:
+                indeks_x -= 1
+
+            nowy_odcinek, czy_w_gore = self.pobierz_trase_wezyka(czy_w_gore,indeks_x)
+            pelna_trasa.extend(nowy_odcinek)
+
+            indeks_x -=2
+        return pelna_trasa
+
+class MetodyMasek:
+    @staticmethod
+    def czy_odwrocic_bit(nr_maski, y, x):
+        if nr_maski == 0:
+            return (y + x) % 2 == 0
+        elif nr_maski == 1:
+            return y % 2 == 0
+        elif nr_maski == 2:
+            return x % 3 == 0
+        elif nr_maski == 3:
+            return (y + x) % 3 == 0
+        elif nr_maski == 4:
+            return ((y // 2) + (x // 3)) % 2 == 0
+        elif nr_maski == 5:
+            return ((y * x) % 2) + ((y * x) % 3) == 0
+        elif nr_maski == 6:
+            return (((y * x) % 2) + ((y * x) % 3)) % 2 == 0
+        elif nr_maski == 7:
+            return (((y + x) % 2) + ((y * x) % 3)) % 2 == 0
+
+
